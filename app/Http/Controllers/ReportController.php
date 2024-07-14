924fbf1ea4ee962787588f56826ac8748963be73
Elmothana <kgl_info@yahoo.com>
2024-07-14 18:56:56 +0200

    2024-07-14

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1006,7 +1006,7 @@ class ReportController extends Controller
 
                 $partner_type_desc = $query->partnership_type == 0 ? 'مستثمر' : 'شريك';
 
-
+dd($total_partner_pulled);
                 DB::table('income_reports')->insert([
                     'id' => $rec_id,
                     'company_id' => session::get('company_id'),
