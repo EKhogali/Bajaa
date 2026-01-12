ff4c686e8a96bd421d4b4ac3b032254962554d05
AqssatCIT <aqssatcit23@gmail.com>
2026-01-12 10:11:58 +0200

    2026-01-12

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -129,8 +129,7 @@
                             @endif
 
                             {{-- التقارير --}}
-                            @if($isAdminOrSupervisor)
-                            <!-- @if($isAdminOrSupervisor || $hasAccountDetailsReportPermission) -->
+                            @if($isAdminOrSupervisor || $hasAccountDetailsReportPermission)
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                                         aria-expanded="false">
